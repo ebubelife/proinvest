7d2727e5fc4534fc7bf380e0e987d67a60740721
ebubelife <ebubeemeka19@gmail.com>
2024-01-02 10:39:50 +0100

    ...

--- database/migrations/2023_12_26_164846_create_members_table.php
+++ database/migrations/2023_12_26_164846_create_members_table.php
@@ -13,6 +13,25 @@ return new class extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->text('name')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('email')->nullable();
+            $table->tinyInteger('email_verified')->default(0);
+            $table->text('photo')->nullable();
+            $table->text('wallet_address')->nullable();
+            $table->text('password')->nullable();
+            $table->float('ref_balance')->default(0);
+            $table->float('balance')->default(0);
+            $table->string('deposit_balance', 20)->default('0.00');
+            $table->string('plan')->nullable();
+            $table->timestamp('plan_updated')->useCurrent();
+            $table->text('ip_address')->nullable();
+            $table->text('location')->nullable();
+            $table->string('referral_code', 10)->nullable();
+            $table->string('referrer1', 10)->nullable()->comment('1st upline');
+            $table->string('referrer2', 11)->nullable()->comment('2nd upline');
+            $table->string('referrer3', 11)->nullable()->comment('3rd upline');
+            $table->text('status')->default("ACTIVE");
             $table->timestamps();
         });
     }
