12818305bd2bcd830205cadb9d1ba25a93a0ea04
ebubelife <ebubeemeka19@gmail.com>
2024-01-03 15:31:16 +0100

    ...

--- database/migrations/2024_01_03_112724_create_transactions_table.php
+++ database/migrations/2024_01_03_112724_create_transactions_table.php
@@ -9,14 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('type', 200)->nullable();
+            $table->string('amount', 200)->nullable();
+            $table->text('asset')->nullable();
+            $table->string('status', 8)->default('PENDING');
+            $table->text('hash')->nullable();
+            $table->text('wallet_address')->nullable();
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
